df696918f387af49ad2d77b03143edffbc1058cf
Fahim090 <FAlSami@grantmatch.com>
2023-03-15 18:53:10 -0600

    backtracking

--- leetcode/backtracking/77-combination_1-to-n.py
+++ leetcode/backtracking/77-combination_1-to-n.py
@@ -1,3 +1,10 @@
+'''
+Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+You may return the answer in any order.
+'''
+
+
 def combine(n, k):
     stack = []
     res = []
