5af72e8445fd37c471619288f3c0bed7fc9506c0
Fahim090 <FAlSami@grantmatch.com>
2023-03-15 20:15:52 -0600

    permutations(backtracking)

---
+++ leetcode/backtracking/40-permutations.py
@@ -0,0 +1,23 @@
+'''
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+'''
+
+
+def permute(arr):
+    result = []
+
+    def backtrack(a):
+        if len(a) == len(arr):
+            result.append(a[:])
+            return
+        else:
+            for i in arr:
+                if i not in a:
+                    a.append(i)
+                    backtrack(a)
+                    a.pop()
+    backtrack([])
+    return result
+
+
+print(permute([0, 1]))
