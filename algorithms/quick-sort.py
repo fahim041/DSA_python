6ed0b06ae37cbe328cd14dfcb3a93af0a7304859
Fahim090 <FAlSami@grantmatch.com>
2023-10-16 20:09:32 -0400

    quicksort

--- algorithms/quick-sort.py
+++ algorithms/quick-sort.py
@@ -1,19 +1,27 @@
 def quick(arr):
-    if len(arr) < 1:
+    def sort(arr, s, e):
+        if e - s + 1 <= 1:
+            return arr
+
+        pivot = arr[e]
+        left = s
+
+        for i in range(s, e):
+            if arr[i] < pivot:
+                tmp = arr[left]
+                arr[left] = arr[i]
+                arr[i] = tmp
+                left += 1
+
+        arr[e] = arr[left]
+        arr[left] = pivot
+
+        sort(arr, s, left - 1)
+        sort(arr, left + 1, e)
+
         return arr
-    else:
-        pivot = arr.pop()
 
-    greater = []
-    smaller = []
-
-    for i in arr:
-        if pivot > i:
-            smaller.append(i)
-        else:
-            greater.append(i)
-
-    return quick(smaller) + [pivot] + quick(greater)
+    return sort(arr, 0, len(arr) - 1)
 
 
 print(quick([5, 2, 5, 7, 12, 3, 6, 8, 2, 5, 7]))
